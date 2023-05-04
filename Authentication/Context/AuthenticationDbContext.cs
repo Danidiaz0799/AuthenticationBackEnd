3aa9a8edc7e52809ef29cf36bcd42a183ebbb8a0
ddsal07 <danidiaz0799@gmail.com>
2023-05-03 21:27:02 -0500

    Agregar archivos de proyecto.

---
+++ Authentication/Context/AuthenticationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Authentication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Authentication.Context
+{
+    public class AuthenticationDbContext : DbContext
+    {
+        public AuthenticationDbContext(DbContextOptions<AuthenticationDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("users");
+        }
+    }
+}
---
+++ Authentication/Controllers/UserController.cs
@@ -0,0 +1,55 @@
+﻿using Authentication.Context;
+using Authentication.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Authentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AuthenticationDbContext _authentificationDbContext;
+        public UserController(AuthenticationDbContext authenticationDbContext)
+        {
+            _authentificationDbContext = authenticationDbContext;
+        }
+
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody] User userObj)
+        {
+            if (userObj == null)
+            {
+                return BadRequest();
+            }
+            var user = await _authentificationDbContext.Users
+                .FirstOrDefaultAsync(x => x.Username == userObj.Username && x.Password == userObj.Password);
+            if (user == null)
+            {
+                return NotFound(new {Message = "User Not Found!" });
+            }
+
+            return Ok(new
+            {
+                Message = "Login Success!"
+            });
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterUser([FromBody] User userObj)
+        {
+            if (userObj == null)
+            {
+                return BadRequest();
+            }
+            await _authentificationDbContext.Users.AddAsync(userObj);
+            await _authentificationDbContext.SaveChangesAsync();
+
+            return Ok(new
+            {
+                Message = "User Registered!"
+            });
+        }
+    }
+}
